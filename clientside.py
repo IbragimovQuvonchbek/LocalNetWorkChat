000120d5e69390164b4580158751fd5cf12204d5
Quvonchbek <147556696+IbragimovQuvonchbek@users.noreply.github.com>
2024-04-25 16:53:56 +0500

    initial

---
+++ clientside.py
@@ -0,0 +1,59 @@
+import socket
+import threading
+import tkinter as tk
+
+SERVER_HOST = '192.168.1.6'
+SERVER_PORT = 12345
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((SERVER_HOST, SERVER_PORT))
+
+
+def receive_messages():
+    while True:
+        message = client_socket.recv(1024).decode()
+        insert_message(message, "left")
+
+
+def insert_message(message, side):
+    if side == "right":
+        tag = "right"
+        background = "lightblue"
+    else:
+        tag = "left"
+        background = "lightgreen"
+
+    chat_text.configure(state=tk.NORMAL)
+    chat_text.tag_configure(tag, justify=tk.RIGHT if side == "right" else tk.LEFT, background=background)
+    chat_text.insert(tk.END, message + "\n", tag)
+    chat_text.configure(state=tk.DISABLED)
+    chat_text.see(tk.END)
+
+
+def send_message(event=None):
+    message = entry_field.get()
+    insert_message(message, "right")
+    client_socket.send(message.encode())
+    entry_field.delete(0, tk.END)
+
+
+client_window = tk.Tk()
+client_window.title("Chat Client")
+client_window.geometry("800x600")
+client_window.resizable(False, False)
+
+chat_text = tk.Text(client_window, wrap=tk.WORD)
+chat_text.pack(expand=True, fill=tk.BOTH)
+
+entry_field = tk.Entry(client_window)
+entry_field.pack(expand=True, fill=tk.X)
+
+send_button = tk.Button(client_window, text="Send", command=send_message)
+send_button.pack()
+
+client_window.bind("<Return>", send_message)
+
+receive_thread = threading.Thread(target=receive_messages)
+receive_thread.start()
+
+client_window.mainloop()
---
+++ serverside.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+SERVER_HOST = '0.0.0.0'
+SERVER_PORT = 12345
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((SERVER_HOST, SERVER_PORT))
+server_socket.listen()
+
+clients = []
+
+
+def handle_client(client_socket):
+    while True:
+        try:
+            message = client_socket.recv(1024).decode()
+        except ConnectionResetError:
+            clients.remove(client_socket)
+            break
+        if not message:
+            break
+        for client in clients:
+            if client != client_socket:
+                client.send(message.encode())
+
+
+def accept_clients():
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"[*] Accepted connection from {client_address}")
+        clients.append(client_socket)
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+
+accept_clients()
